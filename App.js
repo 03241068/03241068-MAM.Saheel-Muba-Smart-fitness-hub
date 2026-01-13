f5c48aa22736203a7f33faee60dd12e7164ad76e
03241068 <03241068@icst.edu.lk>
2026-01-14 00:09:47 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+function calculateBMI() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value / 100;
+
+    let bmi = (weight / (height * height)).toFixed(2);
+    document.getElementById("result").innerHTML = "Your BMI is " + bmi;
+}
+
+document.getElementById("darkModeBtn").addEventListener("click", function() {
+    document.body.classList.toggle("dark");
+});
